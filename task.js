f53a2ac4c7e50db7c11cbcb9479072902d5a6370
Vanshika Agarwal <vanshikagarwal005@gmail.com>
2026-09-07 02:31:38 +0530

    reset button created

---
+++ task.js
@@ -0,0 +1,5 @@
+console.log("task js connected");
+const taskInput = document.querySelector("#task-input");
+const addTaskBtn = document.querySelector("#add-task-btn");
+const pendingTasks = document.querySelector("#pending-tasks");
+
--- water.js
+++ water.js
@@ -3,8 +3,13 @@ const waterAmount=document.querySelector("#water-amount");
 const addWaterBtn = document.querySelector("#add-water-btn");
 const waterProgress=document.querySelector("#water-progress");
 const progress=document.querySelector(".progress");
+const resetWaterBtn=document.querySelector("#reset-water-btn")
 let water=JSON.parse(localStorage.getItem("water"));;
 let waterGoal=2500;
+    waterAmount.textContent=water+"ml";
+    waterProgress.textContent=water+" / "+waterGoal+" ml";
+    let percentage=(water/waterGoal)*100;
+    progress.style.width=percentage+"%";  
 addWaterBtn.addEventListener("click",function(){
     if(water<waterGoal){
     water=water+250;
@@ -15,4 +20,11 @@ addWaterBtn.addEventListener("click",function(){
     let percentage=(water/waterGoal)*100;
     progress.style.width=percentage+"%";
 });
+resetWaterBtn.addEventListener("click",()=>{
+water=0;
+localStorage.removeItem("water");
+waterAmount.textContent=water+"ml";
+waterProgress.textContent=water+" / "+waterGoal+" ml";
+progress.style.width=percentage="0%";
 
+});
