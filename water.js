a9335e9b0fe8c14fa890d23c78fe1cf3abbd70db
Vanshika Agarwal <vanshikagarwal005@gmail.com>
2026-09-07 01:24:02 +0530

    water tracker js code

--- water.js
+++ water.js
@@ -3,15 +3,16 @@ const waterAmount=document.querySelector("#water-amount");
 const addWaterBtn = document.querySelector("#add-water-btn");
 const waterProgress=document.querySelector("#water-progress");
 const progress=document.querySelector(".progress");
-let water=0;
+let water=JSON.parse(localStorage.getItem("water"));;
 let waterGoal=2500;
 addWaterBtn.addEventListener("click",function(){
     if(water<waterGoal){
     water=water+250;
+    localStorage.setItem("water",JSON.stringify(water));
     }
     waterAmount.textContent=water+"ml";
     waterProgress.textContent=water+" / "+waterGoal+" ml";
     let percentage=(water/waterGoal)*100;
-    progress.style.width=percentage;
+    progress.style.width=percentage+"%";
 });
 
